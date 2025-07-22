61cb114f0ff1a39e32255ed3e9b031dfa17771ee
Luis Casas <luisscasas45@gmail.com>
2025-07-21 19:11:50 -0600

    REFAC/ Update EmployerFactory to use faker for company names and generate logo URLs dynamically

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -5,21 +5,18 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employer>
- */
 class EmployerFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+
+        $width = 640;
+        $height = 480;
+        $randomId = $this->faker->numberBetween(1, 1000);
+
         return [
-            'name' => fake()->company(),
-            'logo' => fake()->imageUrl(),
+            'name' => $this->faker->company(),
+            'logo' => "https://picsum.photos/id/{$randomId}/{$width}/{$height}",
             'user_id' => User::factory(),
         ];
     }
